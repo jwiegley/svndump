c7aac176f7596ca7f9146eb474d1bf9b0183ec75
John Wiegley <johnw@newartisans.com>
2012-08-29 16:21:31 -0500

    Updated test to accept file argument

--- test/test-raw.hs
+++ test/test-raw.hs
@@ -4,14 +4,22 @@ import qualified Codec.Compression.GZip as GZip
 import qualified Data.ByteString.Lazy as B
 import           Subversion.Dump.Raw
 import           System.Exit
+import           System.Environment
 
 main :: IO ()
 main = do
-  file <- B.readFile "data/cunit.dump.gz"
-  let len = length $ readSvnDumpRaw (GZip.decompress file)
-  putStrLn $ show len ++ " raw entries found, expecting 1950"
-  if len == 1950
-    then exitSuccess
-    else exitFailure
+  args <- getArgs
+  case args of
+    [] -> do
+      file <- B.readFile "data/cunit.dump.gz"
+      let len = length $ readSvnDumpRaw (GZip.decompress file)
+      putStrLn $ show len ++ " raw entries found, expecting 1950"
+      if len == 1950
+        then exitSuccess
+        else exitFailure
+
+    (fileName:_) -> do
+      contents   <- B.readFile fileName
+      print $ length $ readSvnDumpRaw contents
 
 -- test-raw.hs ends here
